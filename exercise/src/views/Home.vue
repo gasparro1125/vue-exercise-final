cb6de66787174696b6cac37cd03aa2c5c584992c
gasparro1125 <albertogasparrobonett1@gmail.com>
2021-09-07 17:02:49 +0200

    final

--- exercise/src/views/Home.vue
+++ exercise/src/views/Home.vue
@@ -1,24 +1,3 @@
-<!--
-<template>
-  <div class="home">
-    <MembersList />
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import MembersList from "../components/MemberList.vue";
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    MembersList,
-  },
-});
-</script>
--->
-
-
 
 <template>
   <div class="home">
